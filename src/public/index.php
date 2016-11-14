e36758b9bde5bd35d178408844b6fac6ae7d2965
aceawan <ladeveze.quentin@openmailbox.org>
2016-11-14 14:02:07 +0100

    ajout du modèle de carte

--- src/public/index.php
+++ src/public/index.php
@@ -5,14 +5,14 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $db = new \Illuminate\Database\Capsule\Manager;
-$db->addConnection(parse_ini_file('../..//conf/database.conf.ini'));
+$db->addConnection(parse_ini_file('../../conf/database.conf.ini'));
 
 $db->setAsGlobal();
 $db->bootEloquent();
 
 $app = new \Slim\App;
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Plopitiplop");
+    $response->getBody()->write("AAAAAAAAAAAAHHHHHHHHHHHHHHHHHHH");
 
     return $response;
 });
---
+++ src/public/models/Carte.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace models;
+
+class Carte extends \Illuminate\Database\Eloquent\Model{
+	protected $table = "carte";
+	protected $primaryKey = "idcarte";
+	public $timestamps = false;
+}
+?>
