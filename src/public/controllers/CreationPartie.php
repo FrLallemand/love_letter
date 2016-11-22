9d96b0f49fc4417ef77049c12f34eb2e626fb074
aceawan <ladeveze.quentin@openmailbox.org>
2016-11-22 11:38:24 +0100

    vérifie qu'un joueur ne peut pas être dans deux parties en même temps

--- src/public/controllers/CreationPartie.php
+++ src/public/controllers/CreationPartie.php
@@ -4,7 +4,7 @@ namespace controllers;
 use Carbon\Carbon;
 use models\Partie as Partie;
 use models\Joueur as Joueur;
-    
+
 class CreationPartie extends AbstractController{
 
 	public function formCreerPartie($request, $response, $args){
@@ -14,13 +14,13 @@ class CreationPartie extends AbstractController{
 
     public function creerPartie($request, $response, $args){
         date_default_timezone_set('Europe/Paris');
-        //TODO un peu compact...
+
         $joueurs_maximum = $args['joueurs_maximum'];
         $nom_joueur =  $args['nom_joueur'];
-                
+
         //on cherche si il y a une partie en attente de joueurs
         $partie_attente = Partie::where('joueurs_maximum', $joueurs_maximum)
-                        ->whereRaw('joueurs_actuel < joueurs_maximum')                        
+                        ->whereRaw('joueurs_actuel < joueurs_maximum')
                         ->get()
                         ->first();
 
@@ -35,20 +35,29 @@ class CreationPartie extends AbstractController{
         }
         $id_partie = $partie->idpartie;
 
-        //on regarde si le joueur peut prendre ce nom
+        // on regarde si le joueur peut prendre ce nom
         // Pour le moment, la vérification se fait sur les joueurs de la partie
-        //TODO idjoueur doit être l'id de session, pour que le joueur ne puisse pas jouer à deux parties en même temps
+        // TODO idjoueur doit être l'id de session, pour que le joueur ne puisse pas jouer à deux parties en même temps
+        session_start();
+
         $status_nom = '';
         if(Joueur::where('nom', $nom_joueur)
            ->where('idpartie', $id_partie)
            ->exists()){
             $status_nom = 'Nom d\'utilisateur indisponible';
-        }else{
+        } elseif(isset($_SESSION['idjoueur']) && Joueur::where('idjoueur', $_SESSION['idjoueur'])->exists()){
+            $status_nom = 'Le joueur est déjà dans une partie';
+        } else {
+
             //Le nom est libre
             $joueur = new Joueur();
             $joueur->idpartie = $id_partie;
             $joueur->nom = $nom_joueur;
             $joueur->save();
+
+            //On enregistre l'id du joueur dans la session de l'utilisateur
+            $_SESSION['idjoueur'] = $joueur->idjoueur;
+
 
             $partie->joueurs_actuel++;
             switch($partie->joueurs_actuel){
@@ -71,7 +80,7 @@ class CreationPartie extends AbstractController{
 
         $response->getBody()->write(json_encode([
             'id_partie' => $id_partie,
-            'status_nom' =>$status_nom        
+            'status_nom' =>$status_nom
         ]));
         return $response;
     }
